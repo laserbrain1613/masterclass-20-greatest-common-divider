d7eb679190aedd29bcecc8d3661131f4c71d6320
Lars Olani <lars.olani@gmail.com>
2021-01-22 13:27:38 +0100

    Initial commit

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,17 @@
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first >= 10 && second >= 10) {
+            int lowestNumber = Math.min(first, second);
+
+            for (int i = lowestNumber; i > 0; i--) {
+                if ((first % i == 0) & ((second % i == 0))) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+}
